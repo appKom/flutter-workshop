fa918b6792869a62f48bfbab3e6666c221fc2c43
Nexerate <erl1ls@outlook.com>
2024-04-14 10:09:15 +0200

    Name fix

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,6 @@
-import 'package:barbord/home/event_card.dart';
 import 'package:flutter/material.dart';
+
+import 'event_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key, required this.title});
@@ -30,21 +31,21 @@ class HomePage extends StatelessWidget {
                   'Her skal vi hente inn data fra en API',
                   style: TextStyle(fontSize: 24),
                 ),
-                const SizedBox(
+                SizedBox(
                   height: 24,
                 ),
                 Text(
                   'API-et vi skal bruke er https://old.online.ntnu.no/api/v1/event/events/, og vi skal hente ut alle eventene som finnes der. Vi skal vise eventene i en liste, og for hvert event skal vi vise tittel, startdato og et bilde. Vi skal også legge til en knapp for å laste inn flere eventer.',
                   style: TextStyle(fontSize: 24),
                 ),
-                const SizedBox(
+                SizedBox(
                   height: 24,
                 ),
                 Text(
                   'Dere kan style event kortene slik vist nedenfor eller dere kan style deres egne',
                   style: TextStyle(fontSize: 24),
                 ),
-                const SizedBox(
+                SizedBox(
                   height: 24,
                 ),
                 EventCard(),
